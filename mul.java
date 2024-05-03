6fac5356e328b62ab34d9ab7c8a1d7fbd5eecd8b
nandanverma <nandanverma103536@gmail.com>
2024-05-04 00:40:10 +0530

    mul updated

--- mul.java
+++ mul.java
@@ -1,7 +1,8 @@
 public class mul {
     public static void main(String[] args) {
-        int a = 11;
-        int b= 10;
+        int a = 10;
+        int b= 9;
+        //multiplication of two numbers 
         System.out.println("multiplication of "  +a+ "and" +b+ "is " +a*b);
     }
     
