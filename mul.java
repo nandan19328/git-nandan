ce622fdd9b23993d5386da5bd65dffaddaa43ecf
nandanverma <nandanverma103536@gmail.com>
2024-05-04 00:25:01 +0530

    mul java commited

---
+++ mul.java
@@ -0,0 +1,8 @@
+public class mul {
+    public static void main(String[] args) {
+        int a = 2;
+        int b= 3;
+        System.out.println("multiplication of "  +a+ "and" +b+ "is " +a*b);
+    }
+    
+}
