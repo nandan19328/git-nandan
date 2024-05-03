f2ca7136796a41cf99c6fb8a755d6a312f96f535
nandanverma <nandanverma103536@gmail.com>
2024-05-04 03:08:06 +0530

    subtraction code performed

---
+++ sub.java
@@ -0,0 +1,13 @@
+public class sub {
+    public static void main(String[] args) {
+        
+    
+    int a = 100;
+    int b = 23;
+    int c = 2;
+    int subtract = (a-b-c);
+    System.out.println(subtract);
+
+    
+}
+}
