97e09aeda7fdc3df0578f65ceb35cfe22868161e
nandanverma <nandanverma103536@gmail.com>
2024-05-04 02:51:26 +0530

    added addition.java

---
+++ addition.java
@@ -0,0 +1,10 @@
+public class addition {
+    public static void main(String args[]){
+        int a = 100;
+        int b = 12;
+        int c = 200;
+        int add = a+b+c;
+        System.out.println(add);
+    }
+    
+}
