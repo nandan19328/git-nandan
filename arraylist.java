e83133032f85842729c83dd8e30da7b166cb8833
nandanverma <nandanverma103536@gmail.com>
2024-05-04 16:37:28 +0530

    arraylist program created

---
+++ arraylist.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class arraylist {
+    public static void main(String[] args) {
+        ArrayList<String > str = new ArrayList<>();
+        str.add("nandan");
+        str.add("college");
+        str.add("city");
+        System.out.println(str);
+
+        
+    }
+    
+}
